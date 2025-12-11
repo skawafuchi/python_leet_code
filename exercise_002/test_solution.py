4c0029915ba16df3133b42c433207e94aa33f9fd
skawafuchi <skawafuchi@gmail.com>
2025-12-10 18:20:00 -0800

    Reduce redundancy for exercise 2 pytests

--- exercise_002/test_solution.py
+++ exercise_002/test_solution.py
@@ -7,30 +7,21 @@ class TestSolution():
     were provided by LeetCode.com
     """
     solution = Solution()
+
+    def list_comparison(self,list_one:list,list_two:list,solution_list:list)->None:
+        l1 = ListNode().from_list(list_one)
+        l2 = ListNode().from_list(list_two)
+        merged_list = self.solution.addTwoNumbers(l1,l2)        
+        for num in solution_list:
+            assert num == merged_list.val
+            merged_list = merged_list.next
+
     def test_example_1(self):
-        l1 = ListNode().from_list([2,4,3])
-        l2 = ListNode().from_list([5,6,4])
-        sum_list = self.solution.addTwoNumbers(l1,l2)
-        assumed_answer = [7,0,8]
-        for num in assumed_answer:
-            assert num == sum_list.val
-            sum_list = sum_list.next
+        self.list_comparison([2,4,3],[5,6,4],[7,0,8])
 
     def test_example_2(self):
-        l1 = ListNode().from_list([0])
-        l2 = ListNode().from_list([0])
-        sum_list = self.solution.addTwoNumbers(l1,l2)
-        assumed_answer = [0]
-        for num in assumed_answer:
-            assert num == sum_list.val
-            sum_list = sum_list.next
+        self.list_comparison([0],[0],[0])
 
     def test_example_3(self):
-        l1 = ListNode().from_list([9,9,9,9,9,9,9])
-        l2 = ListNode().from_list([9,9,9,9])
-        sum_list = self.solution.addTwoNumbers(l1,l2)
-        assumed_answer = [8,9,9,9,0,0,0,1]
-        for num in assumed_answer:
-            assert num == sum_list.val
-            sum_list = sum_list.next
+        self.list_comparison([9,9,9,9,9,9,9],[9,9,9,9],[8,9,9,9,0,0,0,1])
         
